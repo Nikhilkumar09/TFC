70bf5a9ce02ac6009737da78130eadf62868575e
Nikhilkumar09 <knikhil095@gmail.com>
2026-03-22 17:52:15 +0530

    adding region

--- challenge-1/tf-challenge-1.tf
+++ challenge-1/tf-challenge-1.tf
@@ -1,8 +1,6 @@
 provider "aws" {
-  version    = "~> 2.54"
-  region     = "us-east-1"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+  version = "~> 2.54"
+  region  = "ap-south-1"
 }
 
 provider "digitalocean" {}
